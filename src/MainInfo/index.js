0bd779f08d3fa527f9d30845a13b56bc70b836b1
Sam Brown <stbrown463@gmail.com>
2019-03-21 12:02:59 -0500

    MainInfo styling

--- src/MainInfo/index.js
+++ src/MainInfo/index.js
@@ -7,14 +7,13 @@ const MainInfo = (props) => {
 			<div className="info-pic">
 				<div className="info">
 	        <h1 className="name-main">Sam Brown</h1>
-	        <p className="description-main">I'm a full stack web developer eager to work in a demanding, creative<br /> enviornment that reflects my passion for learning and problem solving.</p>
-	        <p className="email-main">Email: stbrown463@gmail.com</p>
-	        <p className="phone-main">Phone: (312) 208-5611</p>
-	        <p className="location-main">Chicago, IL</p>
-	        <div className="logo-main-flex">
-	          <a href="https://github.com/stbrown463"><p className="social-link">GitHub</p></a>
-	          <a href="https://www.linkedin.com/in/stbrown463/"><p className="social-link">LinkedIn</p></a>
+	        <div className="description-container">
+	        	<p className="description-main">I'm a Chicago based, full stack web developer eager to work in a demanding, creative enviornment that reflects my passion for learning and problem solving.</p>
 	        </div>
+	        <div className="logo-main-flex">
+	          <a className="social-link" href="https://github.com/stbrown463">GitHub</a> | <a className="social-link"href="https://www.linkedin.com/in/stbrown463/">LinkedIn</a> | <a className="social-link" href="mailto:stbrown463@gmail.com">Email</a>
+	        </div>
+	        <p className="phone-main">(312) 208-5611</p>
 	       </div>
       </div>
       <div className="skills">
